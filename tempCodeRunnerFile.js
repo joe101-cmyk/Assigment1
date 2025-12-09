de16ce4655e8e9660c49380ab911d23d818054e3
joe101-cmyk <yousiefnasar@gmail.com>
2025-12-09 23:20:18 +0200

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-function readFileSyncFile(path) {
-    const dataread = fs.readFileSync(path, 'utf8');
-    console.log(dataread);
-}
-const filePath = './file.txt'; 
-readFileSyncFile(filePath);
-//----------------------------
